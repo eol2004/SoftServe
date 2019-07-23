14f882ce719a78eacb8a61f72ac6c1b8cfc20b2b
unknown <eol2004@ukr.net>
2019-07-23 22:45:23 +0300

    Labs

---
+++ ASCII/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace My_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ASCII table");
+            int i = 1;
+         
+                for (i = 1; i < 1256; i++)
+                {
+                    Console.Write(Convert.ToChar(i));
+                 }
+                Console.WriteLine();
+            
+              Console.ReadKey();
+
+        }
+    }
+}
