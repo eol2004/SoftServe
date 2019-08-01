257b6a7ba96cd0f48a6ebaec3227ae9118334782
unknown <eol2004@ukr.net>
2019-08-01 15:40:50 +0300

    РїРёС€Рё С‚СѓС‚ С‡С‚Рѕ С‚С‹ РёР·РјРµРЅРёР»

---
+++ Exchange_two_nums/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exchange_two_nums
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //1
+            byte a = 4;
+            byte b = 7;
+            byte s;
+
+            s = a;
+            a = b;
+            b = s;
+            Console.WriteLine($"a = {a} ; b = {b}");
+
+            //2
+            int sum (byte c, byte d, out byte a1)
+            {
+                a1 = a;
+                return c + d;
+            }
+
+            byte aa1; 
+            a = (byte)(sum(a, b, out aa1) - aa1);
+            b = (byte)(sum(aa1, b, out aa1) - b);
+            Console.WriteLine($"a = {a} ; b = {b}");
+
+            //3
+            byte[] arr1 = null;   
+            arr1 = new byte[2];
+            arr1[0] = 7;
+            arr1[1] = 4;
+            Array.Sort(arr1);
+            Console.WriteLine($" {arr1[0]} ; {arr1[1]}");
+
+            //4
+            void Xchng(ref byte x, ref byte y)
+            {
+                byte z;
+                z = x;
+                x = y;
+                y = z;
+            }
+
+            Xchng(ref a, ref b);
+            Console.WriteLine($"a = {a} ; b = {b}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
+
