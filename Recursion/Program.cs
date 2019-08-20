feefa423a1bd70f397ec374d8b925996f670de4a
unknown <eol2004@ukr.net>
2019-08-20 12:57:56 +0300

    Recursion final

--- Recursion/Program.cs
+++ Recursion/Program.cs
@@ -10,19 +10,59 @@ namespace Recursion
     {
         static void Main(string[] args)
         {
-            //Test t = new Test();
-            //t.s = 6;
-            //Test k = new Test();
-            //k = t;
-            //           int y = 5;
-        //    Console.WriteLine($"{ t},{ k}, {t.s}");
+            Console.Write("Введите количество элементов массива : ");
+            int i = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[i];
+            Randomize(array);
+            Print(array);
+            Console.WriteLine();
+            Console.WriteLine("Результирующий массив");
+            Output(array,0);
             Console.ReadKey();
-
         }
-        //static double Sqr(double x)
-        //{
-        //    return x*x;
-        //}
+
+        public static void Print(int[] array)
+        {
+            Console.WriteLine("Сгенерированный массив");
+            for (int j = 0; j < array.Length; ++j)
+            {
+                Console.Write("{0} ", array[j]);
+            }
+        }
+
+        static void Randomize(int[] array)
+        {
+            Random rnd = new Random();
+            for (int j = 0; j < array.Length; ++j)
+            {
+                array[j] = rnd.Next(-100, 100);
+            }
+        }
+
+        static void Output(int[] array, int j)
+        {
+            if (j <= array.Length - 1) //>        max(j)=array.Length-1
+            {
+                {
+                    if (array[j] < 0 && j <= array.Length - 1) //if negativ & <= max valid index
+                    {
+                        Console.Write("{0} ", array[j]);
+                        Output(array, ++j);
+                    }
+                    else
+                    {
+                        Output(array, ++j);
+                    }
+                }
+            }
+            else
+            {
+                for (int k = 0; k < array.Length; ++k)
+                {
+                    if (array[k] > 0) Console.Write("{0} ", array[k]);
+                }
+            }
+        }
 
     }
 
